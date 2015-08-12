78f590947ac2e8eacea2a1dfe778e383c63eb1fc
João Ferreira <joaov11@hotmail.com>
2015-08-12 16:31:22 -0300

    New Layout - Cadastrar Álbum

---
+++ MusicStore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+   
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Pesquisa_Album()
+        {
+            return View();
+        }
+
+    
+        public ActionResult Cadastra_Album()
+        {
+            return View();
+        }
+    }
+}
