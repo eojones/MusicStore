40582566e9680e31066ef5f594698065eb3b4472
João Ferreira <joaov11@hotmail.com>
2015-08-31 16:00:51 -0300

    cadastra_album (pag1) ok

--- MusicStore/Public/js/views/cadastrarAlbum.js
+++ MusicStore/Public/js/views/cadastrarAlbum.js
@@ -2,6 +2,11 @@
     $('body').attr({
         'data-spy': 'scroll',
         'data-target': '.nav-tabs',
-        'data-offset':'50'
+        'data-offset': '50'
+    });
+    $("#parallax1").height($(document).height());
+
+    $(window).on("resize load", function () {
+        $('section.module.parallax').height($(document).height());
     });
 });
